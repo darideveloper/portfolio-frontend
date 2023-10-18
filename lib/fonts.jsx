28ffdc9d2c4c191c987d709bf7119437101e51c7
daridev <darideveloper@gmail.com>
2023-10-18 15:35:34 -0600

    feat (lib): add fonts

---
+++ lib/fonts.jsx
@@ -0,0 +1,17 @@
+import { Handjet, Tilt_Neon, Oswald } from 'next/font/google'
+
+export const fontAlternative = Handjet({
+  subsets: ['latin'],
+  weights: [400, 700],
+})
+
+export const fontTitle = Tilt_Neon({
+  subsets: ['latin'],
+  weights: [400, 700],
+})
+
+export const fontRegular = Oswald({
+  subsets: ['latin'],
+  weights: [400, 700],
+})
+

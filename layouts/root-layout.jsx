f94f50b320f6223afb353b79d260d228958c64c5
daridev <darideveloper@gmail.com>
2023-10-24 11:36:13 -0600

    feat (layouts): root layout

--- layouts/root-layout.jsx
+++ layouts/root-layout.jsx
@@ -1,14 +1,34 @@
 import { metadata } from "@/lib/metadata"
+import Head from "next/head"
+import Header from "@/sections/header"
+import { fontRegular } from "@/lib/fonts"
 
 export default function RootLayout({ children, extraTitle = "", extraKeywords = [] }) {
   // Get metadata
   const title = metadata.title + (extraTitle && " | " + extraTitle)
-  const keyword = metadata.keywords.concat(extraKeywords).join(", ")
-  
+  const keywords = metadata.keywords.concat(extraKeywords).join(", ")
+
   return (
     <>
-      <head>
-      </head>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="author" content={metadata.author} />
+        <meta name="keywords" content={keywords} />
+      </Head>
+      <Header />
+      <main
+        className={`
+        ${fontRegular.className}
+        mx-auto
+        w-full
+      `}
+      >
+        {children}
+      </main>
+      <footer>
+
+      </footer>
     </>
   )
 }
